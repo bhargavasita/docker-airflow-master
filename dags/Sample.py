6c327435c480fd59437a09ed4f9fcd0cd97b945b
bhargavasita <vippalabhargav.s.r.reddy04@gmail.com>
2025-09-15 12:38:10 -0500

    errors resolved

--- dags/Sample.py
+++ dags/Sample.py
@@ -1,12 +1,12 @@
 from airflow import DAG
 from datetime import datetime
-from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
+
 
 def val_check():
     print("running validation check")
 with DAG(
-    dag_id="First DAG",
-    owner="Airflow",
+    dag_id="First_DAG",
     start_date=datetime(year=2025,month=9,day=15),
     schedule_interval="@daily",
     
@@ -14,6 +14,6 @@ with DAG(
     #task1
     Validation_check=PythonOperator(
         task_id="Val_check",
-        callable="val_check"
+        python_callable=val_check,
     )
 
